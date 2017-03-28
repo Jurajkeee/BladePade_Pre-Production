352948227958c5e8f98f53157b675c0c9c001df1
DESKTOP-6GIQ2FQ\Jura <po4tajurajkeee@gmail.com>
2017-03-28 23:42:04 +0300

    23:41 28.03.2017

--- NINJA ADVENTURES PROTOTYPE/Assets/Assets/Scripts/Shooting2.cs
+++ NINJA ADVENTURES PROTOTYPE/Assets/Assets/Scripts/Shooting2.cs
@@ -5,33 +5,53 @@ public class Shooting2 : MonoBehaviour {
     public GameObject rocket;
     public float speed;
     public KeyCode Fire = KeyCode.F;
-   
-    // Use this for initialization
-    void Start () {
-        
-       
-        
-        
-    }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-	if (Input.GetKeyDown(Fire))
+    public SwordScript shooted;
+    private Animator anims;
+    private float vertical;
+    private float horizontal;
+    private Rigidbody2D body;
+    private int collisionCount = 0;
+
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(Fire))
         {
+
             Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
             Vector2 direction = (Vector2)((worldMousePos - transform.position));
             direction.Normalize();
-
-            // Creates the bullet locally
             GameObject bullet = (GameObject)Instantiate(
                                     rocket,
                                     transform.position + (Vector3)(direction * 0.5f),
                                     Quaternion.identity);
 
-            // Adds velocity to the bullet
+
+
             bullet.GetComponent<Rigidbody2D>().velocity = direction * speed
                 ;
+            bullet.AddComponent<Animator>();
+            bullet.AddComponent<SwordScript>();
+            Physics.IgnoreCollision(bullet.GetComponent<Collider>(), bullet.GetComponent<Collider>());
         }
-	}
+        
+    }
+   
+
+    void Start () {
+
+        anims = GetComponent<Animator>();
+        anims.SetFloat("flying", 2);
+
+        body = GetComponent<Rigidbody2D>();
+        shooted = GetComponent<SwordScript>();
+        
+        
+    }
+    
+
+
+    
+
 }
--- NINJA ADVENTURES PROTOTYPE/Assets/Assets/Scripts/SwordScript.cs
+++ NINJA ADVENTURES PROTOTYPE/Assets/Assets/Scripts/SwordScript.cs
@@ -7,39 +7,38 @@ public class SwordScript : MonoBehaviour
     private float vertical;
     private float horizontal;
     private Rigidbody2D body;
-    public float bulletspeed = 800f;
-    private Rigidbody2D mySword;
+   
+    private int collisionCount = 0;
+    
     public KeyCode Fire = KeyCode.F;
     // Use this for initialization
     void Start()
     {
+        
         anims = GetComponent<Animator>();
         anims.SetFloat("flying", 2);
-        mySword = GetComponent<Rigidbody2D>();
+        
         body = GetComponent<Rigidbody2D>();
-        GameObject Sword = GameObject.Find("Sword");
-        PlayerControl playerControl = Sword.GetComponent<PlayerControl>();
-    }
+        
+        
+        
+}
+    
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.transform.tag == "Ground" || collision.transform.tag == "Side")
-        {
-            body.gravityScale = 0;
-            body.isKinematic = true;
-            anims.SetFloat("flying", -2);
-            float angle = 0;
-            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        }
-
-
-
+        body.gravityScale = 0;
+        body.isKinematic = true;
+        anims.SetFloat("flying", -2);
+        float angle = 0;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
-    
+
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
-
+        
+        
 
     }
 }
